a7ed884a3c653b44f7cf42124e53e24aa6a8b825
Mostafa Azab <60828260+wolfermxm@users.noreply.github.com>
2022-03-09 01:49:48 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,56 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+serverData = [];
+
+// Require Express to run server and routes
+const express=require('express');
+
+// Start up an instance of app
+const app=express();
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors=require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port=3000;
+const server =app.listen(port,listening);
+function listening(){
+    console.log('server is running');
+    console.log(`running on a localhost ${port}`);
+
+}
+
+
+app.post('/addweather', addWeather);
+
+function addWeather (req,res){
+
+    newEntry={
+       temperature:req.body.temperature,
+       date:req.body.date,
+       feeling:req.body.feeling
+
+    }
+    console.log(newEntry);
+    serverData.push(newEntry);
+    projectData=serverData;
+    
+};
+
+// get all data back
+app.get('/all', sendData);
+
+function sendData (request, response) {
+    response.send(projectData);
+  };
+
+  
